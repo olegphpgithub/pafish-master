5e563e064ed04cd1e71e1d979b2328be9498ef5d
olegphpgithub <olegphp@yandex.ru>
2020-02-21 14:52:23 +0300

    Add pafish empty project;

---
+++ vs/stdafx.h
@@ -0,0 +1,13 @@
+
+#pragma once
+
+#include "targetver.h"
+
+#include <Windows.h>
+#include <wininet.h>
+#include <tlhelp32.h>
+#include <psapi.h>
+#include <Shlwapi.h>
+
+#include <stdio.h>
+#include <tchar.h>
